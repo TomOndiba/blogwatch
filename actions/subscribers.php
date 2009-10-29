669c1f6af6d0f1bfcc2c450e28a9f0d357d17dce
Alistair Young <alistair@codebrane.com>
2009-10-29 08:36:08 +0000

    Icon size now depends on number of subscribers

--- actions/subscribers.php
+++ actions/subscribers.php
@@ -5,10 +5,15 @@ global $CONFIG;
 
 $subscribers = get_subscribers(get_input("blog_guid"));
 if ($subscribers != null) {
+	$icon_size = "medium";
+	if (count($subscribers) > 10) {
+		$icon_size = "small";
+	}
+	
 	?><div class="contentWrapper singleview"><?
 	foreach ($subscribers as $user) {
 		echo "<div style=\"float: left; padding: 5px;\">";
-		echo "<a href=\"{$CONFIG->wwwroot}/pg/profile/{$user->username}\"><img src=\"{$user->getIcon("small")}\" title=\"{$user->username}\"/></a><br />";
+		echo "<a href=\"{$CONFIG->wwwroot}/pg/profile/{$user->username}\"><img src=\"{$user->getIcon("".$icon_size."")}\" title=\"{$user->username}\"/></a><br />";
 		echo "{$user->username}";
 		echo "</div>";
 	}
