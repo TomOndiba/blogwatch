4211d6d669951ae269e2ee3d584ba2bc661b3944
Alistair Young <alistair@codebrane.com>
2009-10-29 08:29:00 +0000

    View to extend the profile status page to add a link to your subscriptions

---
+++ views/default/blogwatch/profile.php
@@ -0,0 +1,20 @@
+<?
+ /**
+  * Blogwatch
+  * 
+  * @package Blogwatch
+  * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+  * @author Alistair Young <alistair@codebrane.com>
+  * @copyright codeBrane 2009
+  * @link http://codebrane.com/blog/
+  */
+
+require_once($CONFIG->pluginspath."blogwatch/lib/blogwatchlib.php");
+?>
+<?
+if (user_has_subscriptions($vars['user']->username)) {
+?>
+
+	<input alt="<? echo $vars['url']."action/blogwatch/subscriptions" ?>?username=<? echo $vars['user']->username ?>&height=150&width=400" title="My Subscriptions" class="thickbox" type="button" value="Show my subscriptions" />
+
+<? } ?>
