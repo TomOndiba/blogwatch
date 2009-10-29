50f46b2b9eb1f9bc84822bc9b9356394cc7a2b4a
Alistair Young <alistair@codebrane.com>
2009-10-29 09:23:15 +0000

    Added phpdoc

--- views/default/blogwatch/metatags.php
+++ views/default/blogwatch/metatags.php
@@ -1,26 +1,20 @@
-<?php
+<?
+/**
+ * start
+ * 
+ * @package Blogwatch
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Alistair Young <alistair@codebrane.com>
+ * @copyright codeBrane 2009
+ * @link http://codebrane.com/blog/
+ */
 
-	/**
-	 * Adds metatags to load Javascript required for the profile
-	 * 
-	 * @package ElggProfile
-	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-	 * @author Curverider Ltd <info@elgg.com>
-	 * @copyright Curverider Ltd 2008-2009
-	 * @link http://elgg.com/
-	 * 
-	 */
-
-	/*
-	 * <script type="text/javascript" src="<?php echo $vars['url']; ?>pg/iconjs/profile.js" ></script>
-	 */
-
-?>
-<!--
+/*
+don't need this as Elgg already provides jquery	
 <script type="text/javascript" src="<? echo $vars['url']; ?>mod/blogwatch/lib/js/jquery-latest.pack.js"></script>
--->
+*/
+?>
 <script type="text/javascript" src="<? echo $vars['url']; ?>mod/blogwatch/lib/js/thickbox.js"></script>
-
 <style type="text/css" media="all">
 @import "<? echo $vars['url']; ?>mod/blogwatch/lib/js/thickbox-elgg.css";
 </style>
