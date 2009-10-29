e9d68c9a1dc19a0ae016a6f9319a5801ae3ea40f
Alistair Young <alistair@codebrane.com>
2009-10-29 09:22:16 +0000

    Added phpdoc

--- actions/subscribers.php
+++ actions/subscribers.php
@@ -1,4 +1,14 @@
 <?
+/**
+ * subscribers
+ * 
+ * @package Blogwatch
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Alistair Young <alistair@codebrane.com>
+ * @copyright codeBrane 2009
+ * @link http://codebrane.com/blog/
+ */
+
 require_once($CONFIG->pluginspath."blogwatch/lib/blogwatchlib.php");
 
 global $CONFIG;
