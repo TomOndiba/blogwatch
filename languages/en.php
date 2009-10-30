4c1471341f5132bdd750a03232d27aa2a30f84c6
Alistair Young <alistair@codebrane.com>
2009-10-30 10:49:56 +0000

    Language translations

---
+++ languages/en.php
@@ -0,0 +1,33 @@
+<?
+/**
+ * en
+ * 
+ * @package Blogwatch
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Alistair Young <alistair@codebrane.com>
+ * @copyright codeBrane 2009
+ * @link http://codebrane.com/blog/
+ */
+
+$english = array(
+	// Notification strings
+	"blogwatch:notify:subject" => "notification",
+	"blogwatch:notify:body:header" => "The following post or topic has been updated",
+	"blogwatch:notify:body:footer" => "please note you may have to login before viewing the post or topic.",
+	
+	// Cron output
+	"blogwatch:cron:return:string" => "blogwatch notifications available",
+	
+	// Main blogwatch view
+	"blogwatch:view:blogwatch:subscribe:button" => "Subscribe",
+	"blogwatch:view:blogwatch:unsubscribe:button" => "Unsubscribe",
+	"blogwatch:view:blogwatch:subscribers:button" => "Show subscribers",
+	"blogwatch:view:blogwatch:subscribers:popup:title" => "Subscribers to",
+	
+	// Profile page
+	"blogwatch:view:profile:status:subscriptions:button" => "Show my subscriptions",
+	"blogwatch:view:profile:subscriptions:popup:title" => "My Subscriptions",
+);
+
+add_translation("en", $english);
+?>
