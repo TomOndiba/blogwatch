190cc502ab28a79b28f325d72be0bf9c33c22505
Alistair Young <alistair@codebrane.com>
2009-10-30 10:50:58 +0000

    Localised

--- views/default/blogwatch/profile.php
+++ views/default/blogwatch/profile.php
@@ -10,11 +10,15 @@
  */
 
 require_once($CONFIG->pluginspath."blogwatch/lib/blogwatchlib.php");
+
+// elgg_echo doesn't work inline
+$subscriptions_button_text = elgg_echo("blogwatch:view:profile:status:subscriptions:button");
+$subscriptions_popup_title = elgg_echo("blogwatch:view:profile:subscriptions:popup:title");
 ?>
 <?
 if (user_has_subscriptions($vars['user']->username)) {
 ?>
 
-	<input alt="<? echo $vars['url']."action/blogwatch/subscriptions" ?>?username=<? echo $vars['user']->username ?>&height=150&width=400" title="My Subscriptions" class="thickbox" type="button" value="Show my subscriptions" />
+	<input alt="<? echo $vars['url']."action/blogwatch/subscriptions" ?>?username=<? echo $vars['user']->username ?>&height=150&width=400" title="<? echo $subscriptions_popup_title ?>" class="thickbox" type="button" value="<? echo $subscriptions_button_text ?>" />
 
 <? } ?>
