433c105e50968630a71a9eef8e8ffe7fd53d7691
Alistair Young <alistair@codebrane.com>
2010-03-22 15:51:58 +0000

    Now uses elgg_add_action_tokens_to_url to work with 1.7

--- views/default/blogwatch/blogwatch.php
+++ views/default/blogwatch/blogwatch.php
@@ -37,15 +37,20 @@ if (isloggedin() && ((stristr($_SERVER['REQUEST_URI'], "read")) || (stristr($_SE
 	}
 </script>
 
+<?
+$form_url = elgg_add_action_tokens_to_url($vars['url']."action/blogwatch/form");
+$subs_url = elgg_add_action_tokens_to_url($vars['url']."action/blogwatch/subscribers");
+?>
+
 <div class="contentWrapper singleview">
-	<form name="blogwatch_form" method="post" action="<? echo $vars['url']."action/blogwatch/form" ?>">
+	<form name="blogwatch_form" method="post" action="<? echo $form_url ?>">
 		<? if ($subscribed == "no") { ?>
 			<input name="blogwatch_subscribe_button" type="submit" class="submit_button" value="<? echo $subscribe_button_text ?>" />
 		<? } else { ?>
 			<input name="blogwatch_unsubscribe_button" type="submit" class="submit_button" value="<? echo $unsubscribe_button_text ?>" />
 		<? } ?>
 		<? if (blog_has_subscribers($vars['entity']->getGUID())) { ?>
-			<input alt="<? echo $vars['url']."action/blogwatch/subscribers" ?>?blog_guid=<? echo $vars['entity']->getGUID() ?>&height=300&width=800" title="<? echo $subscribers_pop_title ?> <? echo $vars['entity']->title ?>" class="thickbox" type="button" value="<? echo $subscribers_button_text ?>" />
+			<input alt="<? echo $subs_url; ?>&blog_guid=<? echo $vars['entity']->getGUID() ?>&height=300&width=800" title="<? echo $subscribers_pop_title ?> <? echo $vars['entity']->title ?>" class="thickbox" type="button" value="<? echo $subscribers_button_text ?>" />
 		<? } ?>
 		<input type="hidden" name="blog_guid" value="<? echo $vars['entity']->getGUID() ?>"/>
 		<input type="hidden" name="blog_url" value="<? echo $vars['entity']->getURL() ?>"/>
